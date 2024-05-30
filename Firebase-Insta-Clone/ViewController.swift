5ac067f57a9c52f501482201f046eb743a17db76
Sabricetin <sabriyazilim@gmail.com>
2024-05-30 19:16:17 +0300

    Initial Commit

---
+++ Firebase-Insta-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase-Insta-Clone
+//
+//  Created by Sabri Çetin on 30.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
